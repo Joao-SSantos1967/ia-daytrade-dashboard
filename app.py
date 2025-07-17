0b6dac855b3ffe24623aed081f87dadb7398f1a9
Joao-SSantos1967 <jsssjc@gmail.com>
2025-07-17 20:47:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 from assets.ativos_b3 import ativos_b3
 from assets.ativos_cripto import ativos_cripto
 from assets.ativos_forex import ativos_forex
+from assets.b3 import carregar_ativo as carregar_b3
+from assets.cripto import carregar_ativo as carregar_cripto
+from assets.forex import carregar_ativo as carregar_forex
 
 st.set_page_config(page_title="IA Day Trade", page_icon="📈", layout="centered")
 st.title("📊 IA Assistente para Day Trade Manual")
@@ -10,14 +13,19 @@ tipo_ativo = st.selectbox("Tipo de ativo", ["B3", "Criptomoeda", "Forex"])
 
 if tipo_ativo == "B3":
     ativos = ativos_b3
+    carregar = carregar_b3
 elif tipo_ativo == "Criptomoeda":
     ativos = ativos_cripto
+    carregar = carregar_cripto
 else:
     ativos = ativos_forex
+    carregar = carregar_forex
 
 ticker = st.selectbox("Escolha o ativo", ativos)
 
 st.write(f"Você selecionou: {ticker}")
+
+
 
 
 
