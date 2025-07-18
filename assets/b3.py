2a56b20c11a6963146a08d098b536e27431e6da3
Joao-SSantos1967 <jsssjc@gmail.com>
2025-07-17 22:11:43 -0300

    Create b3.py

---
+++ assets/b3.py
@@ -0,0 +1,12 @@
+import yfinance as yf
+import pandas as pd
+
+def carregar_ativo(ticker):
+    try:
+        df = yf.download(ticker, interval="5m", period="1d")
+        df = df.dropna()
+        return df
+    except Exception as e:
+        print(f"Erro ao carregar ativo {ticker}: {e}")
+        return pd.DataFrame()
+
